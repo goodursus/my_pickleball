375b401b1ed2d2e79ede9982a003d0638984e723
Ursus <goodursus@gmail.com>
2026-02-08 20:21:15 +1100

    Connection timeout

--- src/emailService.js
+++ src/emailService.js
@@ -9,7 +9,7 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: true, // true for 465, false for other ports
+    secure: process.env.SMTP_PORT == 465, // true for 465, false for other ports (587)
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
