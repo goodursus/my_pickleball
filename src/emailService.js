7ce073a362f2158a03ecad357dd3ac5ec4c5f524
Ursus <goodursus@gmail.com>
2026-02-08 22:58:26 +1100

    Use gmail service shortcut for nodemailer

--- src/emailService.js
+++ src/emailService.js
@@ -7,9 +7,7 @@ const nodemailer = require("nodemailer");
 // Ideally, the user would provide SMTP_HOST, SMTP_USER, etc. in environment variables.
 
 const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: process.env.SMTP_PORT == 465, // true for 465, false for other ports (587)
+    service: 'gmail',
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
